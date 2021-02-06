1ff9979419e0bb73e50b7e930d767bba4b20f21d
Mikael Porttila <mikael.porttila@gmail.com>
2021-02-06 16:07:45 +0100

    Added build cmd and removed unused code

--- src/services/language.ts
+++
@@ -1,7 +0,0 @@
-const langDetect = require('langdetect');
-
-export function detectLanguage(text: string): string | null {
-  const detectionResult = langDetect.detect(text);
-  const result = detectionResult.length > 0 ? detectionResult[0].lang : null;
-  return result;
-}
