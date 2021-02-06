496b23eaddced4bbf04d8b1d689e0ca88b9833ec
Mikael Porttila <mikael.porttila@gmail.com>
2021-02-06 16:15:54 +0100

    Rollback fancy new stuff.

--- src/services/client.ts
+++ src/services/client.ts
@@ -1,29 +1,29 @@
 import { Socket } from 'net';
 
 export class Client {
-  #socket: Socket; 
+  private socket: Socket; 
 
   constructor(
     private port: number,
     private host: string,) {
-      this.#socket = new Socket();
+      this.socket = new Socket();
     }
 
     connect(): Socket {
       console.log('Connecting...');
-      return this.#socket.connect(this.port, this.host, () => {
+      return this.socket.connect(this.port, this.host, () => {
         console.log('Connected!');
       });
     }
 
     send(...messages: string[]): void {
       for(const message of messages) {
-        this.#socket.write(`${message}\r\n`);
+        this.socket.write(`${message}\r\n`);
       }
     }
 
     addListener(handler: (message: string) => void): void {
-      this.#socket.addListener('data', (data: Buffer) => {
+      this.socket.addListener('data', (data: Buffer) => {
         if(data && handler) {
           const message = data.toString('utf8');
           handler(message);
